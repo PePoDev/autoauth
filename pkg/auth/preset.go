6ba59fa192256d82405dd04a0eca927032d04dfd
Thiwanon Chomcharoen <gmgame.1525@gmail.com>
2019-11-20 00:02:19 +0700

    refactor struct declaration

--- pkg/auth/preset.go
+++ pkg/auth/preset.go
@@ -23,54 +23,52 @@ func LoadPresetFromPath(dir string, fileName string) AutoAuthPreset {
 	return basePreset.AutoAuth
 }
 
-// MainAutoAuthPreset is used to map to preset file like yml or json file
-type MainAutoAuthPreset struct {
-	AutoAuth AutoAuthPreset `mapstructure:"autoauth"`
-}
+type (
+	// MainAutoAuthPreset is used to map to preset file like yml or json file
+	MainAutoAuthPreset struct {
+		AutoAuth AutoAuthPreset `mapstructure:"autoauth"`
+	}
 
-// AutoAuthPreset is base struct contain all configuration of preset file
-type AutoAuthPreset struct {
-	AutoAuthData `mapstructure:",squash"`
+	// AutoAuthPreset is base struct contain all configuration of preset file
+	AutoAuthPreset struct {
+		AutoAuthData `mapstructure:",squash"`
+		Login        AutoAuthLogin
+		Logout       AutoAuthLogout
+		Heartbeat    AutoAuthHeartbeat
+	}
 
-	Login     AutoAuthLogin
-	Logout    AutoAuthLogout
-	Heartbeat AutoAuthHeartbeat
-}
+	// AutoAuthData contain data for AutoAuth struct
+	AutoAuthData struct {
+		Name      string
+		Encrypted bool
+		Save      []string
+		IsRunning bool
+		Try       int
+	}
 
-// AutoAuthData contain data for AutoAuth struct
-type AutoAuthData struct {
-	Name      string
-	Encrypted bool
+	// AutoAuthLogin contain login preset
+	AutoAuthLogin struct {
+		AutoAuthBaseReuqest `mapstructure:",squash"`
+	}
 
-	Save []string
+	// AutoAuthLogout contain logout preset
+	AutoAuthLogout struct {
+		AutoAuthBaseReuqest `mapstructure:",squash"`
+	}
 
-	IsRunning bool
-	Try       int
-}
+	// AutoAuthHeartbeat contain heartbeat preset
+	AutoAuthHeartbeat struct {
+		AutoAuthBaseReuqest `mapstructure:",squash"`
+		Interval            time.Duration
+		Retry               int
+	}
 
-// AutoAuthLogin contain login preset
-type AutoAuthLogin struct {
-	AutoAuthBaseReuqest `mapstructure:",squash"`
-}
-
-// AutoAuthLogout contain logout preset
-type AutoAuthLogout struct {
-	AutoAuthBaseReuqest `mapstructure:",squash"`
-}
-
-// AutoAuthHeartbeat contain heartbeat preset
-type AutoAuthHeartbeat struct {
-	AutoAuthBaseReuqest `mapstructure:",squash"`
-
-	Interval time.Duration
-	Retry    int
-}
-
-// AutoAuthBaseReuqest is base struct contain data to create request
-type AutoAuthBaseReuqest struct {
-	Endpoint string
-	Method   string
-	Header   []string
-	Body     []string
-	Timeout  time.Duration
-}
+	// AutoAuthBaseReuqest is base struct contain data to create request
+	AutoAuthBaseReuqest struct {
+		Endpoint string
+		Method   string
+		Header   []string
+		Body     []string
+		Timeout  time.Duration
+	}
+)
